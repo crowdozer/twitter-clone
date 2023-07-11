43552d9919a697ce5fc7ad77d917ac8b8fa62bbc
christian <32648607+crowdozer@users.noreply.github.com>
2023-07-10 20:48:43 -0500

    prep user model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Crowdozer',
+            'username' => 'crowdozer',
+            'email' => 'business@crwdzr.io',
+        ]);
     }
 }
